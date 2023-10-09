f2ac0e79681508774baa6a5a1940b3f04f671426
Mehzebin Chowdhury <106879002+doraeem@users.noreply.github.com>
2023-10-09 22:20:52 +0600

    Add files via upload

--- Shape.java
+++ Shape.java
@@ -1,41 +1,60 @@
-abstract class Shape
-{
-    public abstract double calculateArea();
+// Define an interface
+interface Shape {
+    double calculateArea();
 }
-class Circle extends Shape{
+
+// Create classes that implement the interface
+class Circle implements Shape {
     private double radius;
-    public Circle(double radius)
-    {
+
+    public Circle(double radius) {
         this.radius = radius;
-    }  
+    }
+
     @Override
-    public double calculateArea()
-    {
-        return 3.14 * radius* radius;
-    } 
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
 }
-class Rectangle extends Shape
-{
+
+class Rectangle implements Shape {
     private double width;
     private double height;
-    public Rectangle(double width, double height)
-    {
+
+    public Rectangle(double width, double height) {
         this.width = width;
         this.height = height;
     }
+
     @Override
-    public double calculateArea()
-    {
+    public double calculateArea() {
         return width * height;
     }
-
 }
-class main{
-    public static void main(String[] args)
-    {
-        Shape circle = new Circle(5.0);
-        System.out.println( "The Area of Circle is : " + circle.calculateArea());
-        Shape rectangle = new Rectangle(4.0,6);
-        System.out.println("The Area of Rectangle is : " +rectangle.calculateArea());
+
+// Another class that takes a Shape as a parameter in its constructor
+class AreaCalculation {
+    private Shape shape;
+
+    public AreaCalculation(Shape shape) {
+        this.shape = shape;
+    }
+
+    public double calculateArea() {
+        return shape.calculateArea();
+    }
+}
+
+ class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(6, 8);
+
+        AreaCalculation circleCalculation = new AreaCalculation(circle);
+        AreaCalculation rectangleCalculation = new AreaCalculation(rectangle);
+
+        System.out.println("Circle Area: " + circleCalculation.calculateArea());
+        System.out.println("Rectangle Area: " + rectangleCalculation.calculateArea());
+     
     }
 }
